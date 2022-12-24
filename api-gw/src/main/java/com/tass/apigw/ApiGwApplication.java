1bfbd592089e6767d195684b9d366d4e5ae64830
Vu Anh Huy <huy.vu@bluecoglobal.com>
2022-12-24 13:55:08 +0700

    done phan login

--- api-gw/src/main/java/com/tass/apigw/ApiGwApplication.java
+++ api-gw/src/main/java/com/tass/apigw/ApiGwApplication.java
@@ -2,8 +2,10 @@ package com.tass.apigw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ApiGwApplication {
 
     public static void main(String[] args) {
